7ebda983d7193c70325f977a79ce38fb29aef337
Rafael Gutiérrez <abadon.gutierrez@gmail.com>
2013-09-04 00:22:21 -0500

    FizzBuzz kata in Scala

---
+++ FizzBuzz/Scala/src/main/scala/com/jabaddon/katas/fizzbuzz/FizzBuzz.scala
@@ -0,0 +1,13 @@
+package com.jabaddon.katas.fizzbuzz
+
+object FizzBuzz {
+	def main(args: Array[String]) {
+		var i = 0
+		for (i <- 1 to 100) {
+			if (i % 3 == 0 && i % 5 == 0) println("FizzBuzz")
+			else if (i % 3 == 0) println("Fizz")
+			else if (i % 5 == 0) println("Buzz")
+			else println(i)
+		}
+	}
+}
