6989139e0b6508f9ec69f3486b28d00e416262b0
Rafael Gutierrez <abadon.gutierrez@gmail.com>
2015-10-28 23:46:30 -0600

    Bowling Game Kata in Java
    Added the first test, a gutter game.

---
+++ BowlingGame/Java/src/main/java/com/jabaddon/katas/bowlinggame/Game.java
@@ -0,0 +1,11 @@
+package com.jabaddon.katas.bowlinggame;
+
+public class Game {
+    public void roll(int pins) {
+
+    }
+
+    public int score() {
+        return 0;
+    }
+}
--- BowlingGame/Java/src/test/java/com/jabaddon/katas/bowlinggame/BowlingGameTest.java
+++ BowlingGame/Java/src/test/java/com/jabaddon/katas/bowlinggame/BowlingGameTest.java
@@ -2,5 +2,16 @@ package com.jabaddon.katas.bowlinggame;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
 public class BowlingGameTest {
+    @Test
+    public void gutterGame() {
+        Game game = new Game();
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertThat(game.score(), is(0));
+    }
 }
